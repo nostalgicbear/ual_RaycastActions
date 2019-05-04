69fae497c8eeff6bf8bf3ac3f0d57a617616f444
Stephen-PC\Stephen <nostalgicbear@hotmail.com>
2019-05-04 19:09:18 +0100

    Adding examples of triggering actions when the player looks at an object. Can also trigger actions to happen when the player looks away from the object

---
+++ Assets/Scripts/RaycastAction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RaycastAction : MonoBehaviour
+{
+    private RaycastController camerasRaycast; //This is the RaycastController script on the camera
+    private bool hasEventBeenPerformed = false;
+
+
+    public UnityEvent eventToPerformWhenLookedAt; //This is the event that will be performed when the object is looked at
+    public UnityEvent eventToPerfomWhenNotLookedAt; //This is performed when we are no longer looking at the object
+
+
+    private void Start()
+    {
+        camerasRaycast = Camera.main.GetComponent<RaycastController>(); //This finds the raycast controller script on the camera when we run the game. 
+    }
+
+    private void Update()
+    {
+        IsObjectBeingLookedAt(); //This gets called every frame so we are always checking to see if the camera is looking at this object
+    }
+
+    private void IsObjectBeingLookedAt()
+    {
+        if (camerasRaycast.objectWerAreLookingAt == gameObject) //This says 'if the object that the camera is looking at is this object...'
+        {
+            if (hasEventBeenPerformed == false) //This says 'if we have not just performed the actiion'... (we do this because if the camera keeps looking at the object we do not want to perform the action over and over. We just do it once)
+            {
+                eventToPerformWhenLookedAt.Invoke(); //Perform the actions that have set 
+                hasEventBeenPerformed = true; //We then set this variable to be true, so we dont keep performing the action. 
+            }
+        }
+        else { //Else if the camera is NOT looking at this object
+            if(hasEventBeenPerformed == true) //If we have just perfomed the action for when we are looking at the object....
+            {
+                //Anything in here gets called when the camera was looking at the object but has just stopped looking at the object (i.e the camera is now looking away...)
+                eventToPerfomWhenNotLookedAt.Invoke();
+                hasEventBeenPerformed = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RaycastController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RaycastController : MonoBehaviour
+{
+    private RaycastHit hit; //stores the info on the object the raycast hits
+
+    private float rayLength = 100f; //This is the length of the raycast. Make this as long or short as you want. E.g if this is 100, the raycast will cast out 100 units in length
+    // Start is called before the first frame update
+
+    public GameObject objectWerAreLookingAt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PerformRaycast();
+    }
+
+    private void PerformRaycast()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayLength)) //casts a ray from this object (that the script is on) position. Casts the ray in the forward direction. Stores what we hit in the 'hit' object. Casts the ray out for as long as you set the rayLength to be. 
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayLength, Color.green);
+            objectWerAreLookingAt = hit.transform.gameObject;
+
+        }
+        else
+        {
+            //Anything you put in here will get called if you are NOT hitting ANY object with your raycast
+            Debug.DrawRay(transform.position, transform.forward * rayLength, Color.red); //we make the ray red when not hitting anything
+            objectWerAreLookingAt = null;
+        }   
+    }
+}
